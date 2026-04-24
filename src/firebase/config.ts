0548a3d5a0082ecbe9a111b9e4308e43a1083ace
Filip Ocytko <sparklyfrog2060@gmail.com>
2026-04-24 20:41:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5542791276-3d5b5",
+  "appId": "1:519783270726:web:5c43dc3fc542ba3704bd47",
+  "apiKey": "AIzaSyBZP_s9kBMd_HRpKC12OwRnqKGQFs2EBmg",
+  "authDomain": "studio-5542791276-3d5b5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "519783270726"
+};
